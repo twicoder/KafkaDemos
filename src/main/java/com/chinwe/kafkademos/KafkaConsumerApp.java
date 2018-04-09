5289ebf30c59be7638e0dc4f2224311f0c2897b8
twicoder <renqingwei123@163.com>
2018-04-09 09:12:04 +0800

    add demo consumer app

--- src/main/java/com/chinwe/kafkademos/KafkaConsumerApp.java
+++ src/main/java/com/chinwe/kafkademos/KafkaConsumerApp.java
@@ -1,6 +1,9 @@
 package com.chinwe.kafkademos;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,11 +13,17 @@ public class KafkaConsumerApp {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers","localhost:9092,localhost:9093");
-        props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
 
         KafkaConsumer myConsumer = new KafkaConsumer(props);
-        myConsumer.subscribe(Arrays.asList("mytopic"));
+
+        ArrayList<TopicPartition> partitions = new ArrayList<TopicPartition>();
+        TopicPartition myTopicPart0 = new TopicPartition("mytopic",0);
+        partitions.add(myTopicPart0);
+        myConsumer.assign(partitions);
+
+//        myConsumer.subscribe(Arrays.asList("mytopic"));
         // alternatively, use regular expression:
 //        myConsumer.subscribe("my*");
 
@@ -25,6 +34,22 @@ public class KafkaConsumerApp {
 //        myConsumer.subscribe(topics);
 
 //        myConsumer.unsubscribe();
+
+        try{
+            while(true){
+                ConsumerRecords<String, String> records = myConsumer.poll(10);
+                for(ConsumerRecord<String, String> record : records){
+                    // Process each record:
+                    System.out.println(String.format("Topic:%s, Partition: %d, Offset: %d, Key: %s, Value: %s",
+                            record.topic(), record.partition(), record.offset(), record.key(), record.value()));
+                }
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            myConsumer.close();
+        }
+
 
     }
 
